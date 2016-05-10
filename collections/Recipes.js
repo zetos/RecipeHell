94e0bbf2fc7e5bc05955995bf127137e9a4f67da
Zetos <cesar_manoel@hotmail.com>
2016-05-10 18:52:20 -0300

    autoform schema start

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -14,6 +14,9 @@ RecipeShema = new SimpleSchema({
 		label: "Author",
 		autoValue: function() {
 			return this.userId
+		},
+		autoform: {
+			type: "hidden"
 		}
 	},
 	createdAt: {
@@ -21,6 +24,9 @@ RecipeShema = new SimpleSchema({
 		label: "Created At",
 		autoValue: function() {
 			return new Date()
+		},
+		autoform: {
+			type: "hidden"
 		}
 	}
 });
